9b57e879886970d6af615dd564ff1480f4a255af
natkip <natasha.kippur@ucdenver.edu>
2025-05-04 21:33:49 -0600

    Update TasksPage.js

--- frontend/src/pages/TasksPage.js
+++ frontend/src/pages/TasksPage.js
@@ -4,7 +4,7 @@ export default function TasksPage() {
   return (
     <div className="p-4">
       <h2 className="text-xl font-bold mb-2">📋 My Tasks</h2>
-      <p>This is the Tasks page. You can display all user tasks or task history here.</p>
+      <p>This is the Tasks page. In future developments of this app, additional and previous tasks will be displayed here.</p>
     </div>
   );
 }
